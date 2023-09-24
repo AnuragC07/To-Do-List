31963052dadd7c86bf9c3e8ca00697932b88623c
AnuragC07 <anuragforwork0018@gmail.com>
2023-09-24 20:00:24 +0530

    final commit

--- Components/list.js
+++ Components/list.js
@@ -6,25 +6,37 @@ const list = () => {
   const [desc, setDesc] = useState("");
   const [mainTask, setMainTask] = useState([]);
 
-  const submitHandler = (e)=>{
+  const submitHandler = (e) => {
     e.preventDefault();
     console.log(title);
     console.log(desc);
-    setMainTask([...mainTask, {title, desc}]);
+    setMainTask([...mainTask, { title, desc }]);
     setTitle("");
     setDesc("");
   }
+
+  let deleteHandler = (i) => {
+    let copyTask = [...mainTask]
+    copyTask.splice(i,1)
+    setMainTask(copyTask)
+  }
+
 
   let renderTask = <h2>No Tasks Available</h2>
 
   if (mainTask.length > 0) {
     renderTask = mainTask.map((t, i) => {
       return (
-        <li>
-          <div className='flex justify-around m-3'>
-            <h5 className='font-semibold'>{t.title}</h5>
-            <h6 className='font-semibold'>{t.desc}</h6>
-          </div>
+        <li className="flex justify-around m-3">
+          <h5 className='font-semibold'>{t.title}</h5>
+          <h6 className='font-semibold'>{t.desc}</h6>
+          <button onClick={() => {
+            deleteHandler(i)
+          }}
+            className="bg-red-600 text-white px-4 py-2 rounded font-bold"
+          >
+            Remove
+          </button>
         </li>
       );
     });
@@ -33,23 +45,23 @@ const list = () => {
     <>
       <form className='flex justify-center items-center' onSubmit={submitHandler}>
         <input
-         type='text'
-         className='border-black border-2 m-5 px-5 py-2'
-         placeholder='Enter Title Here'
-         value={title}
-         onChange={(e)=>{
+          type='text'
+          className='border-black border-2 m-5 px-5 py-2'
+          placeholder='Enter Title Here'
+          value={title}
+          onChange={(e) => {
             setTitle(e.target.value)
-        }}>
+          }}>
 
         </input>
         <input type='text'
-         className='border-black border-2 m-5 px-5 py-2'
-         placeholder='Enter Description Here'
-         value={desc}
-         onChange={(e)=>{
+          className='border-black border-2 m-5 px-5 py-2'
+          placeholder='Enter Description Here'
+          value={desc}
+          onChange={(e) => {
             setDesc(e.target.value)
-         }}
-         ></input>
+          }}
+        ></input>
         <button className='bg-black text-white py-2 px-4 rounded'>Add Task</button>
       </form>
       <hr></hr>
